65b2260da07ce341dc9fa18708eceb90ac2f8e3e
Scott Smith <mail@ScottHSmith.com>
2016-02-28 09:44:23 -0800

    Fixed bugs.

--- app/routes/starred.js
+++ app/routes/starred.js
@@ -2,8 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        return this.store.query('clip', {
-          star: true
+        // return this.store.query('clip', {
+        //   star: true
+        // });
+        return this.store.filter('clip', function(clip){
+          console.log(clip.star);
+          return clip.get('star');
         });
     },
 
