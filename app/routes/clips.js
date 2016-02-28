790dfe37af834af0d347a05c8d9ff48b4dc3c016
Scott Smith <mail@ScottHSmith.com>
2016-02-28 03:32:59 -0800

    Added firebase, starred route

--- app/routes/clips.js
+++ app/routes/clips.js
@@ -2,19 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        // let clips = [
-        //     {
-        //         title: "Learn Ember",
-        //         url: "http://emberjs.com",
-        //         star: true,
-        //     },
-        //     {
-        //         title: "Facebook Link",
-        //         url: "http://facebook.com",
-        //         star: false,
-        //     }
-        // ];
-        // return clips;
         return this.store.findAll('clip');
     }
 });
--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'clipit',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://clipit.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
